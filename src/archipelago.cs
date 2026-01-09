70e15b5e672b094de1a03e6e3032c516fe85d2a9
Andrew K <53620706+Andrewki44@users.noreply.github.com>
2026-01-10 02:47:48 +1100

    Poptracker Auto Tabbing DataStorage (#6)

--- src/archipelago.cs
+++ src/archipelago.cs
@@ -15,6 +15,7 @@ using System.Runtime.InteropServices;
 //using Fahrenheit.Modules.ArchipelagoFFX.GUI;
 using static Fahrenheit.Modules.ArchipelagoFFX.ArchipelagoData;
 using static Fahrenheit.Modules.ArchipelagoFFX.Client.FFXArchipelagoClient;
+using Archipelago.MultiClient.Net.Enums;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 using System.Text.RegularExpressions;
@@ -518,6 +519,8 @@ public unsafe partial class ArchipelagoFFXModule : FhModule {
             on_map_change();
             last_room_id = Globals.save_data->current_room_id;
             last_entrance_id = Globals.save_data->current_spawnpoint;
+
+            FFXArchipelagoClient.current_session?.DataStorage[Scope.Slot, "FFX_ROOM"] = last_room_id;
         }
     }
 
